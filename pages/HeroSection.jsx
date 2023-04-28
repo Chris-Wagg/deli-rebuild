023a322f7d4e1099ee481dd043637359552fbeac
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-04-28 10:54:32 +0200

    hero section skeleton added

--- pages/HeroSection.jsx
+++ pages/HeroSection.jsx
@@ -1,7 +1,21 @@
 export default function HeroSection() {
 	return (
 		<>
-			<section className="hero-container"></section>
+			<section className="hero-container">
+				<h1 className="hero-text">Den mysiga Delin på Gärdet</h1>
+				<h2 className="info-text">
+					Smit in för en frukost, lunch eller middag. Vi har öppet alla dagar i
+					veckan. Varmt välkommen!
+				</h2>
+				<section className="opening-hours-container">
+					<p className="opening-hours-text">
+						<p>Öppet:</p>
+						<p>måndag - fredag 8-21</p>
+						<p>lördag 11-21</p>
+						<p>söndag 11-20</p>
+					</p>
+				</section>
+			</section>
 		</>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import LeftDrawer from '../components/LeftDrawer'
 import NavBar from '../components/NavBar'
+import HeroSection from './HeroSection'
 
 export default function Home() {
 	return (
 		<>
 			<NavBar />
+			<HeroSection />
 			{/* <LeftDrawer /> */}
 		</>
 	)
