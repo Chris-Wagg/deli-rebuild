e915453a9bbdb0dfe1875e7a0394d6425e01d65b
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-04-28 09:35:06 +0200

    basic drawer added with no links

---
+++ components/TemporaryDrawer.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+
+export default function TemporaryDrawer() {
+	const [state, setState] = React.useState({
+		left: false,
+	})
+
+	const toggleDrawer = (anchor, open) => (event) => {
+		setState({ ...state, [anchor]: open })
+	}
+
+	const list = (anchor) => (
+		<Box
+			sx={{
+				width: 250,
+			}}
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		></Box>
+	)
+
+	return (
+		<div>
+			{['left'].map((anchor) => (
+				<React.Fragment key={anchor}>
+					<button onClick={toggleDrawer(anchor, true)}>Menu</button>
+					<Drawer
+						anchor={anchor}
+						open={state[anchor]}
+						onClose={toggleDrawer(anchor, false)}
+					>
+						{list(anchor)}
+						<button>jeff</button>
+					</Drawer>
+				</React.Fragment>
+			))}
+		</div>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
-import DropdownMenu from '../components/DropdownMenu'
-import NavBar from '../components/NavBar'
-import NavItem from '../components/NavItem'
+import TemporaryDrawer from '../components/TemporaryDrawer'
 
 export default function Home() {
-	return (
-		<NavBar>
-			<NavItem icon="Home" />
-			<NavItem icon="Menu's">
-				<DropdownMenu></DropdownMenu>
-			</NavItem>
-			<NavItem icon="Catering" />
-			<NavItem icon="Contact us" />
-		</NavBar>
-	)
+	return <TemporaryDrawer />
 }
