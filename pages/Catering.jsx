40dfe5143d28b75a04d4ef211d9fdc74263b9c8a
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-05-04 10:29:07 +0200

    buffet items changed to lists

--- pages/Catering.jsx
+++ pages/Catering.jsx
@@ -8,53 +8,58 @@ export default function Catering() {
 					{/* put these in a details to take up less space */}
 					<details>
 						<summary>STORA FRUKOSTPAKETET</summary>
-						<p>Bokas 2 dagar innan,minimum 8 pers mellan 8.00-10.30</p>
-						<p>- Surdegsfralla med ost / skinka / kalkon</p>
-						<p>- Apelsinjuice / Äppeljuice</p>
-						<p>- Fruktsallad</p>
-						<p>- Overnight Oats med banan & nötter</p>
-						<p>- Smoothie</p>
-						<p>- Kaffe / Te</p>
-						<p>125kr</p>
+						<ul>
+							<p>Bokas 2 dagar innan,minimum 8 pers mellan 8.00-10.30</p>
+							<li> Surdegsfralla med ost / skinka / kalkon</li>
+							<li> Apelsinjuice / Äppeljuice</li>
+							<li> Fruktsallad</li>
+							<li> Overnight Oats med banan & nötter</li>
+							<li> Smoothie</li>
+							<li> Kaffe / Te</li>
+							<p>125kr</p>
+						</ul>
 					</details>
 				</div>
 				<div className="buffet-container">
-					<div className="small-buffet">
-						<details>
-							<summary>LILLA BUFFEN</summary>
-							<p>- Fetaostpaj</p>
-							<p>- Skinka & ostpaj</p>
-							<p>- Skivad Serrano</p>
-							<p>- Picklad rödlök</p>
-							<p>- Rödpest</p>
-							<p>- Blandad sallad med 2 sorters dressing</p>
-							<p>- Bröd och smör</p>
+					<details>
+						<summary>LILLA BUFFEN</summary>
+						<ul>
+							<li> Fetaostpaj</li>
+							<li> Skinka & ostpaj</li>
+							<li> Skivad Serrano</li>
+							<li> Picklad rödlök</li>
+							<li> Rödpest</li>
+							<li> Blandad sallad med 2 sorters dressing</li>
+							<li> Bröd och smör</li>
 							<p>175 kr per person</p>
-						</details>
-					</div>
+						</ul>
+					</details>
+					<details>
+						<summary>Small Student buffet</summary>
+						<ul>
+							<li>Skagen canapé</li>
+							<li>Varmrökt lax med forellromskräm</li>
+							<li>Marinerad rostbiff med rostad lök </li>
+							<li>Krämig potatissallad med senap och kapris</li>
+							<li>Ädelostsallad med päron & valnötter</li>
+							<li>Surdegsbröd med vispat brynt smör </li>
+							<li>Chokladmousse med vispad grädde och färska bär</li>
+							<p>245 kr per person minst 10 personer</p>
+						</ul>
+					</details>
+					<details>
+						<summary>Large Student buffet</summary>
+						<ul>
+							<p>everything from the Small buffet plus the following</p>
+							<li>+ Tunnbrödsnitt med kallrökt lax & färskost</li>
+							<li>+ Honungsglaserade morötter med rostad bovete</li>
+							<li>+ Pastasallad med picklad gurka & dill</li>
+							<li>+ Rädisor med färskostdipp </li>
+							<li>+ Blandad grönsallad</li>
+							<p>295 kr per person minst 10 personer</p>
+						</ul>
+					</details>
 				</div>
-				<details>
-					<summary>Small Student buffet</summary>
-					<p>Skagen canapé</p>
-					<p>Varmrökt lax med forellromskräm</p>
-					<p>Marinerad rostbiff med rostad lök </p>
-					<p>Krämig potatissallad med senap och kapris</p>
-					<p>Ädelostsallad med päron & valnötter</p>
-					<p>Surdegsbröd med vispat brynt smör </p>
-					<p>Chokladmousse med vispad grädde och färska bär</p>
-					<p>245 kr per person minst 10 personer</p>
-				</details>
-				<details>
-					<summary>Large Student buffet</summary>
-					<p>everything from the Small buffet plus the following</p>
-					<p>+ Tunnbrödsnitt med kallrökt lax & färskost</p>
-					<p>+ Honungsglaserade morötter med rostad bovete</p>
-					<p>+ Pastasallad med picklad gurka & dill</p>
-					<p>+ Rädisor med färskostdipp </p>
-					<p>+ Blandad grönsallad</p>
-					<p>295 kr per person minst 10 personer</p>
-					<p></p>
-				</details>
 			</section>
 		</>
 	)
