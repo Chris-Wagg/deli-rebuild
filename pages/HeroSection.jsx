6f59dc7beea16c12992f329ccd6212b2f6da5189
Chris-Wagg <cgwagg@hotmail.co.nz>
2023-06-06 11:37:49 +0200

    prisma schema setup and TS package installed

--- pages/HeroSection.jsx
+++ pages/HeroSection.jsx
@@ -3,30 +3,30 @@ import LeftDrawer from '../components/LeftDrawer'
 export default function HeroSection() {
 	return (
 		<>
-			<section className="hero-container">
+			<section className='hero-container'>
 				{/* logo should go here */}
-				<div className="text-container">
-					<h1 className="hero-text">Den mysiga Delin på Gärdet</h1>
-					<h2 className="info-text">
-						Smit in för en frukost, lunch eller middag. Vi har öppet alla dagar
-						i veckan. Varmt välkommen!
+				<div className='text-container'>
+					<h1 className='hero-text'>Den mysiga Delin på Gärdet</h1>
+					<h2 className='info-text'>
+						Smit in för en frukost, lunch eller middag. Vi har öppet
+						alla dagar i veckan. Varmt välkommen!
 					</h2>
-					<div className="opening-hours-text">
+					<div className='opening-hours-text'>
 						<ul>
 							Öppet:
 							<li>Måndag - Tisdag 8-15</li>
 							<li>Onsdag - Fredag 8-21</li>
-							<li>Lördag 11-21</li>
-							<li>Söndag 11-20</li>
+							<li>Lördag 10-21</li>
+							<li>Söndag 10-20</li>
 						</ul>
 					</div>
-					<div className="button-container">
+					<div className='button-container'>
 						<LeftDrawer />
 						<button>Catering</button>
 					</div>
 				</div>
-				<div className="image-container">
-					<img src="food-3.jpg" alt="food-1-image" />
+				<div className='image-container'>
+					<img src='food-3.jpg' alt='food-1-image' />
 				</div>
 			</section>
 		</>
--- pages/MenuStuff.jsx
+++ pages/MenuStuff.jsx
@@ -6,9 +6,12 @@ export default function MenuStuff() {
 					<h1 className="header-text">
 						Frukostmeny | måndag - fredag klockan 8-11
 					</h1>
-					<p>Coffee, juices, smoothies and more!</p>
-					<p>fralla, yoghurt and some hot food as well!</p>
-					<p>Swing on by and start your day right at the funkiest of Deli's!</p>
+					<h1 className="header-text">
+						Brunchmeny | Lördag - Söndag klockan 10-15
+					</h1>
+					<h1 className="header-text">
+						Kvellsmeny | Tisdag - Söndag klockan 17-21
+					</h1>
 				</div>
 				<div className="image-container">
 					<img src="food-2.jpg" alt="Second food image" />
